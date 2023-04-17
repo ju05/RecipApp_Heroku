895a7db9d210defd0aef8deb3130c53199c35af3
Juliana <j.amaralwork@gmail.com>
2023-04-17 11:54:45 +0300

    google login settings added to admin

--- RecipApp/settings.py
+++ RecipApp/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xFC0XVNJhCWKR8ZdM6jG7-9TmtjdFkz5ThJWWNHiirf0UPbmRvei5w'
 
-print('loaded settings')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -27,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['warm-dusk-28049.herokuapp.com']
+ALLOWED_HOSTS = ['warm-dusk-28049.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

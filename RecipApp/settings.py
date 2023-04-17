0d578541c74c43afd3fa8e1d5235d269f8dc285d
Juliana <j.amaralwork@gmail.com>
2023-04-17 10:15:55 +0300

    heroku db configurations

--- RecipApp/settings.py
+++ RecipApp/settings.py
@@ -97,10 +97,10 @@ WSGI_APPLICATION = 'RecipApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipeapp_heroku',
-        'USER':'postgres',
-        'PASSWORD': '1948',
-        'HOST': 'localhost',
+        'NAME': 'd68dq15a42f444',
+        'USER':'mcbgkwgigfihvt',
+        'PASSWORD': '74f790679537dcd4c23c57fad718c7a772b3d047ab7d9c5ff640db29a802edf7',
+        'HOST': 'ec2-3-234-204-26.compute-1.amazonaws.com',
         'PORT': '5432'
     }
 }
